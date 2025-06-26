7fb394efde9e7ff2fce21c7fecf1854724daa960
Chrischam2830 <chris.chambros@gmail.com>
2025-06-26 07:48:43 -0700

    Update and rename test_imports.py to imports

--- test_imports.py
+++
@@ -1,6 +0,0 @@
-# test_imports.py
-try:
-    from moviepy.editor import VideoFileClip
-    print("✅ MoviePy installed and importable!")
-except ModuleNotFoundError:
-    print("❌ MoviePy NOT installed!")
