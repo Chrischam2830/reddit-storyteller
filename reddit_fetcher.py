7485a663789c713c2962d27a1200d7a5322adfc5
Chrischam2830 <chris.chambros@gmail.com>
2025-06-26 14:12:03 -0700

    Update reddit_fetcher.py

--- reddit_fetcher.py
+++ reddit_fetcher.py
@@ -1,25 +1,21 @@
-import random
 import praw
+import random
 
 def fetch_top_post(config):
     reddit = praw.Reddit(
-        client_id=config['reddit']['client_id'],
-        client_secret=config['reddit']['client_secret'],
-        username=config['reddit']['username'],
-        password=config['reddit']['password'],
-        user_agent=config['reddit']['user_agent']
+        client_id=config["reddit"]["client_id"],
+        client_secret=config["reddit"]["client_secret"],
+        username=config["reddit"]["username"],
+        password=config["reddit"]["password"],
+        user_agent=config["reddit"]["user_agent"],
     )
 
-    # Pick a random subreddit from the list
-    subreddits = config['reddit']['subreddits']
-    chosen_sub = random.choice(subreddits)
-    subreddit = reddit.subreddit(chosen_sub)
+    subs = config["reddit"]["subreddits"]
+    random.shuffle(subs)
 
-    for submission in subreddit.hot(limit=10):
-        if not submission.stickied and len(submission.selftext) > 100:
-            return {
-                "title": submission.title,
-                "body": submission.selftext
-            }
+    for sub in subs:
+        for submission in reddit.subreddit(sub).hot(limit=20):
+            if not submission.stickied and submission.selftext and len(submission.selftext) > 100:
+                return {"title": submission.title, "body": submission.selftext}
 
     raise Exception("No suitable post found.")
