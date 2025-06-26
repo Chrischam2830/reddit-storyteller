896383ce582e4d5379a3a44be023cef09613a116
Chrischam2830 <chris.chambros@gmail.com>
2025-06-26 10:54:46 -0700

    Update reddit_fetcher.py

--- reddit_fetcher.py
+++ reddit_fetcher.py
@@ -1,15 +1,25 @@
+import random
 import praw
 
 def fetch_top_post(config):
     reddit = praw.Reddit(
         client_id=config['reddit']['client_id'],
         client_secret=config['reddit']['client_secret'],
-        user_agent=config['reddit']['user_agent'],
         username=config['reddit']['username'],
-        password=config['reddit']['password']
+        password=config['reddit']['password'],
+        user_agent=config['reddit']['user_agent']
     )
-    subreddit = reddit.subreddit(config['reddit']['subreddit'])
+
+    # Pick a random subreddit from the list
+    subreddits = config['reddit']['subreddits']
+    chosen_sub = random.choice(subreddits)
+    subreddit = reddit.subreddit(chosen_sub)
+
     for submission in subreddit.hot(limit=10):
         if not submission.stickied and len(submission.selftext) > 100:
-            return {'title': submission.title, 'body': submission.selftext}
+            return {
+                "title": submission.title,
+                "body": submission.selftext
+            }
+
     raise Exception("No suitable post found.")
