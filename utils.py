ee3a201612c0e32bbed5e221b63b6ef26333e3af
Chrischam2830 <chris.chambros@gmail.com>
2025-06-26 10:34:59 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,21 @@
-import yaml
+import os
 
-def load_config(path):
-    with open(path, "r") as f:
-        return yaml.safe_load(f)
+def load_config():
+    return {
+        "reddit": {
+            "client_id": os.getenv("REDDIT_CLIENT_ID"),
+            "client_secret": os.getenv("REDDIT_SECRET"),
+            "username": os.getenv("REDDIT_USERNAME"),
+            "password": os.getenv("REDDIT_PASSWORD"),
+            "user_agent": os.getenv("REDDIT_USER_AGENT"),
+            "subreddits": [s.strip() for s in os.getenv("REDDIT_SUBREDDITS", "AskReddit").split(",")]
+        },
+        "openai": {
+            "api_key": os.getenv("OPENAI_API_KEY"),
+            "organization": os.getenv("OPENAI_ORGANIZATION")
+        },
+        "elevenlabs": {
+            "api_key": os.getenv("ELEVEN_API_KEY"),
+            "voice_id": os.getenv("ELEVEN_VOICE_ID")
+        }
+    }
